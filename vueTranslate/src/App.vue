f6f09b05bf07e4b75735f148ddfbea3d0e06de0a
WWongs <wangzhao90hou@163.com>
2018-08-01 11:03:06 +0800

    push vueTranslate

---
+++ vueTranslate/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h1>在线翻译</h1>
+    <h5 class="text-muted">简单 / 易用 / 便捷</h5>
+    <translateform v-on:submitValue="translateText"></translateform>
+    <translateOutput v-bind:translatedText="translatedText" style="margin-top: 20px;"></translateOutput>
+  </div>
+</template>
+
+<script>
+import translateform from './components/Translateform'
+import translateOutput from './components/TranslateOutput'
+export default {
+  name: 'App',
+  data:function(){
+  	return {
+  		translatedText:''
+  	}
+  },
+  components: {
+    translateform,
+    translateOutput
+  },
+  methods:{
+  	translateText:function(text,language){
+  		let url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180801T004315Z.4674ff432316dab3.c1f7db7cc5729751f0511e1a935fd508d41edf77&lang='+language+'&text=';
+  		this.$http.get(url+text).then((response)=>{
+  			this.translatedText = response.body.text[0];
+  		});
+  	}
+  }
+}
+
+
+
+console.log("This people is out of his mind!!!");
+console.log("Black People???");
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueTranslate/src/components/TranslateForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row" id="translateForm">
+  	<div class="col-md-12 col-md-offset-2">
+	    <form class="well el-form-item__content" style="width: 50%;margin: 10px auto;">
+	    	<input class="form-control" type="text" placeholder="请输入需要翻译的内容" v-model="textToTranslate"/>
+	    	<select v-model="language" class="form-control" style="width: 30%;margin: 10px auto;">
+	    		<option value="en">English</option>
+	    		<option value="ru">Russian</option>
+	    		<option value="ko">Korean</option>
+	    		<option value="ja">Janpenese</option>
+	    	</select>
+	    	<input type="submit" value="翻译" class="btn btn-primary" @click="formSubmit"/>
+	    </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'translateForm',
+  components: {
+    
+  },
+  data:function(){
+  	return {
+  		textToTranslate:'',
+  		language:''
+  	}
+  },
+  methods:{
+  	formSubmit:function(e){
+  		this.$emit("submitValue",this.textToTranslate,this.language);
+  		e.preventDefault();	
+  	}	
+  },
+  created:function(){
+  		this.language = 'en';
+  	}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
